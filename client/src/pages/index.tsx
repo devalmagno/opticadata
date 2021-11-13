4e19d22cf68524265fa16c87b87f1a05c11416ee
devalmagno <devalmagno@gmail.com>
2021-11-13 17:53:44 -0300

    Login page is done

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { KeyboardEvent, useRef, useState } from "react";
+import { FormEvent, KeyboardEvent, useRef, useState } from "react";
+import { api } from "../services/api";
 
 import styles from "./login.module.scss";
 
@@ -7,18 +8,34 @@ const Home = () => {
 
     const [isManager, setIsManager] = useState(false);
     const [isCapsLockOn, setIsCapslockOn] = useState(false);
+    const [isWord, setIsWord] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    const [cpf, setCpf] = useState('');
+    const [password, setPassword] = useState('');
+    const [acessToken, setAcessToken] = useState('');
 
     const handleManager = () => {
       setIsManager(!isManager);
     }
 
-    const handleCPFInput = () => {
-        if (cpfRef.current?.value.length == 3 || cpfRef.current?.value.length == 7) {
-            cpfRef.current.value += ".";
-        }
+    const handleCPFInput = (keyEvent: KeyboardEvent) => {
+        if (keyEvent.key != 'Backspace' && keyEvent.key != 'Tab' && keyEvent.key != 'Enter') {
+            if (!/\d/.test(keyEvent.key)) {
+                setIsWord(true);
+                return;
+            } 
+    
+            setIsWord(false);
 
-        if (cpfRef.current?.value.length == 11) {
-            cpfRef.current.value += "-";
+
+            if (cpfRef.current?.value.length == 3 || cpfRef.current?.value.length == 7) {
+                cpfRef.current.value += ".";
+            }
+    
+            if (cpfRef.current?.value.length == 11) {
+                cpfRef.current.value += "-";
+            }   
         }
     }
 
@@ -27,10 +44,31 @@ const Home = () => {
         else setIsCapslockOn(false);
     }
 
+    const handleLogin = (e: FormEvent) => {
+        e.preventDefault();
+
+        api.post('managers/login', {
+            cpf,
+            password
+        }).then(response => {
+            alert('Login realizado com sucesso!');
+
+            const { acessToken } = response.data;
+
+            setAcessToken( acessToken ); 
+        }).catch(() => {
+            setIsError(true)
+        });
+    }
+
     return (
         <div className={styles.container}>
+            <div className={styles.logo}>
+                <img src="/logoopticodata.svg" alt="OpticoData" />
+            </div>
+
             <div className={styles.login}>
-                <h2>Bem vindo ao OpticoData</h2>
+                <h2>Bem vindo ao ÓpticoData</h2>
                 <div className={styles.user}>
                     { isManager ? <h3>Gerente</h3> : <h3>Funcionario</h3> }
 
@@ -39,18 +77,32 @@ const Home = () => {
                       { isManager ? 'Funcionário' : 'Gerente' }
                     </strong>
                 </div>
-                <form action="#">
-                        <div className={styles.inputBox}>
+                <form onSubmit={handleLogin}>
+                        <div className={`${styles.inputBox} ${styles.cpf}`}>
                             <input
                                 ref={cpfRef}
+                                
                                 type="text"
                                 name="cpf"
                                 id="cpf"
                                 placeholder="CPF"
+                                pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"
+                                autoComplete="off"
                                 maxLength={14}
-                                onKeyDown={handleCPFInput}
+                                onKeyDown={keyEvent => handleCPFInput(keyEvent)}
+                                value={cpf}
+                                onChange={e => { setCpf(e.target.value) }}
                                 required
                             />
+
+                            {
+                                isWord &&
+                                (
+                                    <div className={`${styles.warning} ${styles.isword}`}>
+                                        <strong>Apenas números são permitidos.</strong>
+                                    </div>  
+                                )
+                            }
                         </div>
                         <div className={styles.inputBox}>
                             <input
@@ -59,14 +111,25 @@ const Home = () => {
                                 id="password"
                                 placeholder="Senha"
                                 onKeyDown={keyEvent => handleCapslock(keyEvent)}
+                                value={password}
+                                onChange={e => { setPassword(e.target.value) }}
                                 required
                             />
                             { 
                                 isCapsLockOn &&
                                 (
-                                    <div className={styles.capslock}>
+                                    <div className={`${styles.warning} ${styles.capslock}`}>
                                         <strong>CapsLock ativado</strong>
                                     </div>      
+                                )
+                            }
+
+{
+                                isError &&
+                                (
+                                    <div className={`${styles.warning} ${styles.isword}`}>
+                                        <strong>CPF ou senha errado.</strong>
+                                    </div>
                                 )
                             }
                         </div>
@@ -75,6 +138,11 @@ const Home = () => {
                             <input type="submit" value="Entrar" />
                         </div>
                     </form>
+            </div>
+
+            <div className={styles.footer}>
+                Desenvolvido por
+                <img src="/logonorteminas.svg" alt="Norte Minas Soluções Digitais" />
             </div>
         </div>
     );
